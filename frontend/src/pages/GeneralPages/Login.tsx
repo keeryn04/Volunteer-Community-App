f59215b9185b14ac11352a3e90f0a9c44b9ba7df
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2025-11-08 16:02:26 -0700

    FE: FEAT - Basic Header
    Basic header with title, nav options, and profile button (non function).

---
+++ frontend/src/pages/GeneralPages/Login.tsx
@@ -0,0 +1,16 @@
+// src/pages/Home.tsx
+import React from "react";
+import Header from "../../Components/GeneralComponents/Header";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <main className="p-6">
+        LOGIN PAGE CONTENT
+      </main>
+    </div>
+  );
+};
+
+export default Home;
