eb9e8e95049d95e8106f6dc53c363111a7d6bb8d
Cody Casselman <cody.casselman@ucalgary.ca>
2025-11-08 17:12:51 -0700

    FE: FEAT - Added API endpoints to FE

---
+++ frontend/src/api/user/user.api.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import type LoginResponse from '../../interfaces/api/response/LoginResponse';
+import type PagesResponse from '../../interfaces/api/response/PagesResponse';
+
+const API_BASE_URL = '/api';
+
+export async function userLogin(username: String, password: String): Promise<LoginResponse | undefined>{
+    try{
+        const response = await axios.get(`${API_BASE_URL}/user/login`, {
+            params: {
+                username: username,
+                password: password
+            }
+        })
+        return response.data;
+    }catch(error){
+        console.error('Error', error);
+    }
+}
+
+export async function getPages(userId: string): Promise<PagesResponse|undefined>{
+    try{
+        const response = await axios.get(`${API_BASE_URL}/${userId}/pages`);
+        return response.data;
+    }catch(error){
+        console.error('Error', error);
+    }
+}
