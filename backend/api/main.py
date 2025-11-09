2a19dc28b166c26b856c19797787a5f6dea99fa3
keeryn04 <keerynjohnson@gmail.com>
2025-11-08 17:09:13 -0700

    BE: FEAT - Basic Route Setup

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,11 +5,6 @@ app = FastAPI()
 app.include_router(router)
 
 #Test code 
-@app.get("/api/test")
-def get_test():
-    return "test"
-
-#Test with parameter
-@app.get("/api/test/{param}")
-def get_test_param(param: str):
-    return f"test with param: {param}"
+@app.get("/")
+def get_health():
+    return "Backend Active!"
